84ee136434f36b04c8f80aa3926ce07dab4a3e22
ERKAM GURESEN <erkamguresen@gmail.com>
2021-07-18 12:23:28 -0700

    not working tutorial as always

--- src/features/todos/TodoListItem.js
+++ src/features/todos/TodoListItem.js
@@ -1,53 +1,30 @@
 import React from 'react'
-
-import { ReactComponent as TimesSolid } from './times-solid.svg'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { availableColors, capitalize } from '../filters/colors'
 
-const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
+const selectTodoById = (state, todoId) => {
+  return state.todos.find((todo) => todo.id === todoId)
+}
+
+// Destructure `props.id`, since we only need the ID value
+const TodoListItem = ({ id }) => {
+  // Call our `selectTodoById` with the state _and_ the ID value
+  const todo = useSelector((state) => selectTodoById(state, id))
   const { text, completed, color } = todo
 
-  const handleCompletedChanged = (e) => {
-    onCompletedChange(e.target.checked)
+  const dispatch = useDispatch()
+
+  const handleCompletedChanged = () => {
+    dispatch({ type: 'todos/todoToggled', payload: todo.id })
   }
 
-  const handleColorChanged = (e) => {
-    onColorChange(e.target.value)
-  }
-
-  const colorOptions = availableColors.map((c) => (
-    <option key={c} value={c}>
-      {capitalize(c)}
-    </option>
-  ))
+  // omit other change handlers
+  // omit other list item rendering logic and contents
 
   return (
     <li>
-      <div className="view">
-        <div className="segment label">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={completed}
-            onChange={handleCompletedChanged}
-          />
-          <div className="todo-text">{text}</div>
-        </div>
-        <div className="segment buttons">
-          <select
-            className="colorPicker"
-            value={color}
-            style={{ color }}
-            onChange={handleColorChanged}
-          >
-            <option value=""></option>
-            {colorOptions}
-          </select>
-          <button className="destroy" onClick={onDelete}>
-            <TimesSolid />
-          </button>
-        </div>
-      </div>
+      <div className="view">{/* omit other rendering output */}</div>
     </li>
   )
 }
